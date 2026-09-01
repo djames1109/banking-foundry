8d6fb679f7e3609937666e1d98b117d6980eb1f1
DJames <davejames1109@gmail.com>
2026-09-02 07:45:48 +0800

    Initialize coverbill spring app

---
+++ coverbill/src/main/java/com/castle/djames/bankingfoundry/coverbill/CoverBillApplication.java
@@ -0,0 +1,12 @@
+package com.castle.djames.bankingfoundry.coverbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoverBillApplication.class, args);
+    }
+}
